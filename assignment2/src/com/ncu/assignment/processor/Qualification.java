6e9c11752942b567727cd824a18382b33cf98e9d
AashimaSachdeva <sachdevaaashima81@gmail.com>
2019-02-07 18:41:14 +0530

    added assignment2

---
+++ assignment2/src/com/ncu/assignment/processor/Qualification.java
@@ -0,0 +1,15 @@
+class Qualification
+{
+	String qualName;
+	String university;
+	String institute;
+	float cgpa;
+
+	void setQual (String qname, String uni, String insti, float p)
+	{
+		this.qualName = qualName;
+		this.university = university;
+		this.institute = institute;
+		this.cgpa = cgpa;
+	}
+}
